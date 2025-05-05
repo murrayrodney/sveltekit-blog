6dbeb6e0a43af12894e2661cc0cc65119f0e8d52
murrayrodney <murray.rodney.7@gmail.com>
2025-05-04 19:06:25 -0600

    Start adding some skills and styling them

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,128 @@
-<h1>Hello from Rodney</h1>
+<h1>Rodney Murray</h1>
+
+<div class="grid w-15 grid-cols-2">
+	<a
+		href="https://www.linkedin.com/in/rodney-murray-308780a2/"
+		target="_blank"
+		rel="noopener noreferrer"
+		style="display: flex; align-items: center; text-decoration: none;"
+	>
+		<img
+			src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/linkedin.svg"
+			alt="LinkedIn"
+			style="width: 24px; height: 24px; margin-right: 8px; filter: invert(1);"
+		/>
+	</a>
+
+	<a
+		href="https://github.com/murrayrodney"
+		target="_blank"
+		rel="noopener noreferrer"
+		style="display: flex; align-items: center; text-decoration: none;"
+	>
+		<img
+			src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/github.svg"
+			alt="GitHub"
+			style="width: 24px; height: 24px; margin-right: 8px; filter: invert(1);"
+		/>
+	</a>
+</div>
+
+<h2>Engineer/Data Scientist</h2>
+
+<p>
+	I am a data scientist who started off as a petroleum engineer in Alaska after graduating from the
+	University of Wyoming with a degree in Chemical Engineering. After working as an engineer for
+	several years I discovered my love for working with data problems which lead me to a data science
+	based role (Data Analytics Engineer in our company). After several months of working in that
+	position and recognizing an opportunity to build a better foundation in statistics I began the
+	Master of Applied Statistics program from Colorado State University.
+</p>
+
+<h2>Skills</h2>
+
+<ul class="skills">
+	<li class="skill">
+		<h3>Python</h3>
+		<p>
+			Use of Python for scientific computing, machine learning, and data science workflows. Familiar
+			with libraries such as Numpy, Pandas, Keras/Tensorflow/ PyMC, Scikit-learn
+		</p>
+	</li>
+	<li class="skill">
+		<h3>R</h3>
+		<p>
+			Use of R for statistical modeling and analysis of experiments. Familiar with the Tidyverse,
+			survival modeling, and mixed modeling libraries.
+		</p>
+	</li>
+	<li class="skill">
+		<h3>SQL</h3>
+		<p>Querying, summarizing, and manipulating large datasets</p>
+	</li>
+	<li class="skill">
+		<h3>Statistics</h3>
+		<p>
+			Application of statistics in the engineering domain to uncover important relationships and
+			inform decisions. Significant utilization of both tradtional (frequentist) and Bayesian
+			methods for modeling and analysis.
+		</p>
+	</li>
+	<li class="skill">
+		<h3>Maching learning</h3>
+		<p>
+			Implementation of machine learning algorithms to make better and faster predictions with an
+			emphasis in integration of physics based processes.
+		</p>
+	</li>
+	<li class="skill">
+		<h3>Reinforcement Learning</h3>
+		<p>Leveraged for waterflood optimization with custom built environments.</p>
+	</li>
+	<li class="skill">
+		<h3>AWS</h3>
+		<p>
+			Use AWS services to integrate new data sources into datalakes and make available to other
+			engineers.
+		</p>
+	</li>
+	<li class="skill">
+		<h3>Reservoir Engineering</h3>
+		<p>
+			Expertise in optimization of existing production and injection patterns as a part of complex
+			integrated surface networks and extremely faulted reservoirs.
+		</p>
+	</li>
+	<li class="skill">
+		<h3>Production Engineering</h3>
+		<p>
+			Optimization of producing and injecting wells with a focus on complex integrated surface
+			networks.
+		</p>
+	</li>
+</ul>
+
+<style>
+	h2 {
+		margin-top: 30px;
+		border-top: 2px solid var(--border);
+		width: 100vw;
+	}
+
+	.skills {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 16px;
+
+		.skill {
+			border: 2px solid var(--border);
+			padding: 15px;
+			border-radius: 15px;
+		}
+
+		.skill:hover {
+			transform: scale(1.05);
+			background-color: var(--surface-4); /* Fallback color added */
+		}
+	}
+</style>
